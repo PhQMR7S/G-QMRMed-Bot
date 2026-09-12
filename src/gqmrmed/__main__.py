3ba193d29134852968f3da32178467cb39d20004
PhQMR7S <323991773+PhQMR7S@users.noreply.github.com>
2026-09-13 01:02:29 +0300

    fix: format application entrypoint

--- src/gqmrmed/__main__.py
+++ src/gqmrmed/__main__.py
@@ -5,4 +5,9 @@ from gqmrmed.config import get_settings
 
 if __name__ == "__main__":
     settings = get_settings()
-    uvicorn.run("gqmrmed.main:app", host="0.0.0.0", port=8000, reload=settings.app_env == "development")
+    uvicorn.run(
+        "gqmrmed.main:app",
+        host="0.0.0.0",
+        port=8000,
+        reload=settings.app_env == "development",
+    )
