fc33fb7e46393e6f9f4dfd5d5f4028cb6f8a817f
PhQMR7S <323991773+PhQMR7S@users.noreply.github.com>
2026-09-13 01:22:36 +0300

    style: normalize import block spacing

--- src/gqmrmed/db/base.py
+++ src/gqmrmed/db/base.py
@@ -4,7 +4,6 @@ from uuid import UUID, uuid4
 from sqlalchemy import DateTime, func, MetaData, Uuid
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 
-
 NAMING_CONVENTION = {
     "ix": "ix_%(column_0_label)s",
     "uq": "uq_%(table_name)s_%(column_0_name)s",
