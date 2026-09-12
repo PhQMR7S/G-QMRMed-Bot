4a0f3f789e1e2db30eda52291c9aaf4cad98b75b
PhQMR7S <323991773+PhQMR7S@users.noreply.github.com>
2026-09-13 01:23:24 +0300

    test: tighten activation-code alphabet assertion

--- tests/test_activation.py
+++ tests/test_activation.py
@@ -5,6 +5,6 @@ from gqmrmed.services.activation import generate_activation_code
 
 def test_activation_code_is_human_readable_and_high_entropy() -> None:
     code = generate_activation_code("PLUS")
-    assert re.fullmatch(r"GQMR-PLUS-[A-Z2-9]{6}-[A-Z2-9]{6}", code)
+    assert re.fullmatch(r"GQMR-PLUS-[A-HJ-NP-Z2-9]{6}-[A-HJ-NP-Z2-9]{6}", code)
     assert len(code.split("-")[-1]) == 6
     assert not any(character in code for character in "01IO")
