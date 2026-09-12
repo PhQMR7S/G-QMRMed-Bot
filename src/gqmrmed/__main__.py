a0607f60c05016add724394864f2e7080d715161
PhQMR7S <323991773+PhQMR7S@users.noreply.github.com>
2026-09-13 00:46:54 +0300

    chore: add executable application entrypoint

---
+++ src/gqmrmed/__main__.py
@@ -0,0 +1,8 @@
+import uvicorn
+
+from gqmrmed.config import get_settings
+
+
+if __name__ == "__main__":
+    settings = get_settings()
+    uvicorn.run("gqmrmed.main:app", host="0.0.0.0", port=8000, reload=settings.app_env == "development")
